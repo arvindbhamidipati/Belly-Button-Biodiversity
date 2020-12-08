0b0f4218d04c3893e3bdc4ce3837cd98eb4ac4ca
Arvind Bhamidipati <arvindbhamidipati@users.noreply.github.com>
2020-12-07 23:37:00 -0800

    bar chart works

---
+++ StarterCode/app.js
@@ -0,0 +1,90 @@
+console.log(d3.json("samples.json"))
+
+function gpbar(id) {
+  d3.json("samples.json").then(data =>{
+    //console.log(data)
+    var ids = data.samples[0].otu_ids
+    //console.log(ids)
+    //cut the data into the top 10. Graph should only show top 10 in reverse order
+    var sampleValues =  data.samples[0].sample_values.slice(0,10).reverse();
+    //console.log(sampleValues)
+    var data_labels =  data.samples[0].otu_labels.slice(0,10);
+    //console.log(data_labels)
+    var OTU_topTen = (data.samples[0].otu_ids.slice(0, 10)).reverse();
+    //console.log(OTU_topTen)
+    var OTU_id = OTU_topTen.map(d => "OTU " + d);
+    //console.log(OTU_id)
+    var labels =  data.samples[0].otu_labels.slice(0,10);
+    //console.log(labels)
+    console.log(`OTU IDS: ${OTU_id}`)
+    console.log(`OTU_labels: ${labels}`)
+
+    var structure = {
+            x: sampleValues,
+            y: OTU_id,
+            text: data_labels,
+            marker: {
+            color: 'blue'},
+            type:"bar",
+            orientation: "h",
+        };
+
+    var data = [structure]
+
+    Plotly.newPlot("bar", data);
+})}
+
+gpbar();
+
+
+//---------------------------------- Bubble Chart-------------------- //
+
+function gpbubble(id) {
+  d3.json("samples.json").then(data =>{
+    //console.log(data)
+    var ids = data.samples[0].otu_ids
+    //console.log(ids)
+    //cut the data into the top 10. Graph should only show top 10 in reverse order
+    var sampleValues =  data.samples[0].sample_values.slice(0,10).reverse();
+    //console.log(sampleValues)
+    var data_labels =  data.samples[0].otu_labels.slice(0,10);
+    //console.log(data_labels)
+    var OTU_topTen = (data.samples[0].otu_ids.slice(0, 10)).reverse();
+    //console.log(OTU_topTen)
+    var OTU_id = OTU_topTen.map(d => "OTU " + d);
+    //console.log(OTU_id)
+    var labels =  data.samples[0].otu_labels.slice(0,10);
+    //console.log(labels)
+    console.log(`OTU IDS: ${OTU_id}`)
+    console.log(`OTU_labels: ${labels}`)
+
+    var trace1 = {
+            x: sampledata.samples[0].otu_ids,
+            y: sampledata.samples[0].sample_values,
+            mode: "markers",
+            marker: {
+                size: sampledata.samples[0].sample_values,
+                color: sampledata.samples[0].otu_ids
+            },
+            text:  sampledata.samples[0].otu_labels
+
+        };
+
+        // set the layout for the bubble plot
+        var layout_2 = {
+            xaxis:{title: "OTU ID"},
+            height: 600,
+            width: 1000
+        };
+
+        // creating data variable
+        var data1 = [trace1];
+
+    // create the bubble plot
+    Plotly.newPlot("bubble", data1, layout_2);
+
+    });
+    var data = [structure]
+
+    Plotly.newPlot("bubble", data);
+})}
